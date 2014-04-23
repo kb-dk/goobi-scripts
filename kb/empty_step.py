39638b7757d8c1a31738c7777c3caebd407d289c
Ronan McHugh <mchugh.r@gmail.com>
2014-04-23 11:37:11 +0200

    Add placeholder step as development stub

---
+++ kb/empty_step.py
@@ -0,0 +1,19 @@
+from goobi.goobi_step import Step
+
+class EmptyStep( Step ):
+
+	def setup(self):
+		self.name = 'A tester'
+		self.config_main_section = 'test'
+		self.essential_config_sections = set( [] )
+		self.essential_commandlines = {
+			'process_id' : 'number',
+			'process_path' : 'folder'
+		}
+
+	def step(self):
+		return None
+
+if __name__ == '__main__':
+	
+	EmptyStep( "config/config.ini" ).begin()
