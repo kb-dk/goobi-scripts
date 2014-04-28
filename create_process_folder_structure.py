0ec870aae631ce930f0560a2b03b1d6603569708
Jeppe Eimose Waagstein <jeel@kb.dk>
2014-04-28 10:16:13 +0200

    Stub for create procees folder structure.

---
+++ create_process_folder_structure.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+'''
+Created on 28/04/2014
+
+@author: jeel
+'''
+import os
+
+from goobi.goobi_step import Step
+
+class create_process_folder_structure( Step ) :
+
+    def setup(self):
+    
+        self.name = "Create complete folder structure"
+        self.config_main_section = "create_process_folder_structure"
+    
+    def step(self):
+        
+        
+if __name__ == '__main__' :
+    create_process_folder_structure().begin()
