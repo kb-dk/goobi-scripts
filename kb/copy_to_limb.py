7b1490134f45e941d62fd7a352136cddffbba8c5
davidgrove73 <dgj@kb.dk>
2014-04-28 10:41:18 +0200

    moved creation of transit dir inside try-except change errorlogging a little

--- kb/copy_to_limb.py
+++ kb/copy_to_limb.py
@@ -22,13 +22,9 @@ class CopyToLimb( Step ):
 			transit_dir = os.path.join(self.getConfigItem('limb_transit'),os.path.basename(source_dir))
 			hotfolder_dir = self.getConfigItem('limb_hotfolder')
 			sleep_interval = self.getConfigItem('sleep_interval')
-
-			print transit_dir
-			print hotfolder_dir
 									
 			self.info_message("source_dir "+source_dir)
-			os.makedirs(transit_dir)
-			
+						
 			"""
 			attempt to copy all files to LIMB transit dir
 			retry until all files are copied
@@ -36,26 +32,31 @@ class CopyToLimb( Step ):
 			src_files = [[l,False] for l in os.listdir(source_dir)]			
 			while (files_not_copied):
 				files_not_copied = False
-				for src_file in src_files:
-					self.info_message("starting copy")
-					if (not src_file[1]):
-		    				full_file_name = os.path.join(source_dir, src_file[0])
-		    				if (os.path.isfile(full_file_name)):
-					   		try:	
+				try: 
+					"""
+					create transit dir
+					"""
+					if not os.path.exists(transit_dir):
+						os.makedirs(transit_dir)
+
+					for src_file in src_files:
+						self.info_message("starting copy")
+						if (not src_file[1]):
+			    				full_file_name = os.path.join(source_dir, src_file[0])
+			    				if (os.path.isfile(full_file_name)):					   			
 								shutil.copy2(full_file_name, transit_dir)
 								src_file[1] = True
-					   		except Exception:
-								self.info_message("Error copying file '"+full_file_name)
-								files_not_copied = True
-						else:
-							self.info_message(full_file_name+" is not af file")
+							else:
+								self.info_message(full_file_name+" is not af file")
+				except Exception as e:
+					self.info_message("Error copying files: %s'" %e)
+					files_not_copied = True
 				if (files_not_copied):
 					self.info_messages("Not all files copied. Sleeping ...")
 					time.sleep(sleep_interval)				
 			shutil.move(transit_dir,hotfolder_dir)
 		except Exception as e:
-			print e
-			self.info_message("An error happened ")			
+			self.info_message("An error happened %s" %e)			
 		return None
 
 if __name__ == '__main__':	
