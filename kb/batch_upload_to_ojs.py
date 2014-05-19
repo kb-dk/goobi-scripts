d8ee5c4964606a563250c07f8b7971719aa0028b
Ronan McHugh <mchugh.r@gmail.com>
2014-05-19 16:23:24 +0200

    more work on batch upload, parse process title, better argument parsing

--- kb/batch_upload_to_ojs.py
+++ kb/batch_upload_to_ojs.py
@@ -1,6 +1,8 @@
 import sys, os
-import tools.tools as tools
+import ConfigParser
 from optparse import OptionParser
+
+import tools.tools as tools
 
 def main():
 	'''
@@ -18,30 +20,48 @@ def doUpload(options):
 		# skip any non-directory files
 		if not os.path.isdir(abs_path): continue
 
-		config = options.config if options.config else None
-		args = buildArgs(abs_path, config)
+		args = buildArgs(abs_path, options)
 		uploadFiles(args)
 		runImportScript(args)
 		
 
-def buildArgs(abs_path, config):
+def buildArgs(abs_path, options):
 	'''
 	Generate the arguments that will be used by both scripts
 	'''
-	args = "process_id=1 process_path={0} process_title=lan-01-2008".format(abs_path)
-	# if we haven't supplied a config, goobi step will try to guess one
-	if config: args += " config_path={0}".format(os.path.abspath(config))
+	abs_config = os.path.abspath(options.config)
+	process_title = getProcessTitle(abs_path, abs_config)
+	args = "process_id=1 process_path={0} process_title={1} config_path={2}"\
+		.format(abs_path, process_title, abs_config)
+	# if there are options for debug settings, add it
+	if options.debug: args += " debug=true"
 	return args
 
 def uploadFiles(args):
 	'''
-	build the args for the script
+	call upload step with args
 	'''
 	print 'python upload_to_ojs.py {0}'.format(args)
 	os.system('python upload_to_ojs.py {0}'.format(args))
+	sys.exit(0)
 
-def runImportScript(path):
-	print "running import script"
+def runImportScript(args):
+	'''
+	Call import script with args
+	'''
+	print 'python run_ojs_import.py {0}'.format(args)
+	os.system('python run_ojs_import.py {0}'.format(args))
+
+def getProcessTitle(path, config_file):
+	'''
+	We assume that the process title for the given directory
+	is the same as the name of the toc file minus .toc
+	'''
+	config = ConfigParser.RawConfigParser()
+	config.read(config_file)
+	toc_dir = os.path.join(path, config.get('process_folder_structure', 'metadata_toc_path'))
+	toc_name = tools.getFirstFileWithExtension(toc_dir, 'toc')
+	return toc_name[:-4]
 
 def getOptions():
 	try:
@@ -49,6 +69,7 @@ def getOptions():
 		parser.add_option('-d', '--dir', dest='root_dir', help='The directory containing all the process directories.')
 		parser.add_option('-c', '--config', dest='config', 
 			help='The config file to be read by the scripts. If not specified, the system default will be assumed.')
+		parser.add_option('-v', '--debug', dest='debug', help='Should the scripts run in debug mode?')
 		(options, args) = parser.parse_args()
 		ensureValidOptions(options, parser)
 		tools.ensureDirsExist(options.root_dir)
@@ -61,10 +82,18 @@ def getOptions():
 
 def ensureValidOptions(options, parser):
 	if not options.root_dir:
-		print "We can't do this without a root directory..."
-		print parser.print_help()
-		sys.exit(1)
+		exitWithError("We can't do this without a root directory...", parser)
+	if not options.config:
+		exitWithError("We cant do this without a config file...", parser)
+		
+	# if we have a debug argument - convert it to boolean
+	if options.debug:
+		options.debug = (options.debug.lower() == 'true')
 
+def exitWithError(msg, parser):
+	print msg
+	print parser.print_help()
+	sys.exit(1)
 
 if __name__ == '__main__':
 	main()
