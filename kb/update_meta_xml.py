3d0033b0a7bd5eecbe7d09f8ec5462432cc5fced
Ronan McHugh <mchugh.r@gmail.com>
2014-07-25 13:55:35 +0200

    new step

---
+++ kb/update_meta_xml.py
@@ -0,0 +1,30 @@
+from goobi.goobi_step import Step
+
+
+class EmptyStep( Step ):
+
+	def setup(self):
+		self.name = 'A tester'
+		self.config_main_section = 'test'
+		self.essential_config_sections = set( [] )
+		self.essential_commandlines = {
+			'process_id' : 'number',
+			'process_path' : 'folder'
+		}
+
+	def step(self):
+		return None
+
+	def getVariables(self):
+		'''
+		This script pulls in all the variables
+		from the command line and the config file 
+		that are necessary for its running.
+		Errors in variables will lead to an 
+		Exception being thrown.
+		'''
+		return None
+
+if __name__ == '__main__':
+	
+	EmptyStep( ).begin()
