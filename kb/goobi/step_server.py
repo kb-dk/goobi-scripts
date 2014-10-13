435b6cab12aa81e34c5064ea250c8532ef2f3250
Jeppe Eimose Waagstein <jeel@kb.dk>
2014-10-13 11:06:22 +0200

    Changed log-messages for step_server, changed getSettings in goobi_step and updated run_ojs_import to new metadata structure.

--- kb/goobi/step_server.py
+++ kb/goobi/step_server.py
@@ -39,13 +39,16 @@ class ConvertServer():
         log_path = '/opt/digiverso/logs/step_server/' 
         log_level = 'INFO'
         self.logger = logger.logger(log_path,log_level)
+        self.logger.log_section('Setting up step server')
         # Setup host and port for connection
         host = 'localhost'
         port = 37000
         self.address = (host, port)
         # Create one processor per core - tesseract 3.03 is not multithreaded
         # Todo: Add functionality so first thread will always take from non shared
-        # queue, and the other always from the shared. E.g.
+        # queue, and the others always from the shared.
+        
+        # TODO: add core_num to a configuration file
         self.core_num = 2
         self.step_job_processors = []
         
@@ -56,7 +59,7 @@ class ConvertServer():
             self.logger.info('Initiating step job processor {0}...'.format(i+1))
             self.step_job_processors.append(StepJobProcessor(shared_job_queue=self.job_queue,
                                                              logger=self.logger))
-        self.logger.info('Step job processor started.')
+        self.logger.info('Step job processor(s) started.')
         self.server = StepJobTCPServer(server_address = self.address,
                                        RequestHandlerClass = StepJobTCPHandler,
                                        bind_and_activate=True,
@@ -67,25 +70,26 @@ class ConvertServer():
         self.logger.log_section('Starting step job processor(s)...')
         for step_processor in self.step_job_processors:
             step_processor.start()
-        self.logger.info('Step job processor(s) started.')
-        self.logger.info('Starting step job server...')
+            self.logger.info('Step job processor started.')
+        self.logger.info('Step server started, awaiting jobs...')
         signal.signal(signal.SIGTERM, self.signal_term_handler)
         try:
             self.server.serve_forever()
         except KeyboardInterrupt:
             msg = 'KeyboardInterrupt called. Closing server gently.'
-            self.logger.info(msg)
+            self.logger.log_section(msg)
         except Exception as e:
             err = 'An error occured: {0}. Closing server gently.'
             err = err.format(str(e))
-            self.logger.error(err)
+            self.logger.log_section(err,log_level='ERROR')
         self.server.server_close()
         self.logger.info('Server closed.')
-        self.logger.info('Closing {0} step job processor(s).'.format(len(self.step_job_processors)))
+        self.logger.log_section('Stopping {0} step job processor thread(s).'.format(len(self.step_job_processors)))
         for step_processor in self.step_job_processors:
             step_processor.stop()
             step_processor.join()
-        self.logger.info('{0} step job processor(s) stopped.'.format(len(self.step_job_processors)))
+            self.logger.info('{0} step job processor thread stopped.')
+        self.logger.info('{0} step job processor thread(s) stopped.'.format(len(self.step_job_processors)))
         self.logger.log_section('Existing server.')
 
 if __name__ == "__main__":
--- kb/run_ojs_import.py
+++ kb/run_ojs_import.py
@@ -4,80 +4,82 @@ from goobi.goobi_step import Step
 import os, subprocess
 import tools.goobi.metadata as goobi_tools
 import tools.tools as tools
+from tools.mets import mets_tools
 from tools.errors import DataError
 
 
 
 class RunOJSImport( Step ):
 
-	def setup(self):
-		self.config_main_section = 'ojs'
-		self.essential_config_sections = set( ['ojs'] )
-		self.essential_commandlines = {
-			'process_id' : 'number',
-			'process_path' : 'folder',
-			'process_title' : 'string'
-		}
+    def setup(self):
+        self.config_main_section = 'ojs'
+        self.essential_config_sections = set( ['ojs'] )
+        self.essential_commandlines = {
+            'process_id' : 'number',
+            'process_path' : 'folder',
+            'process_title' : 'string'
+        }
 
-	def step(self):
-		'''
-		This script logs into the OJS server
-		and runs the PHP script for import 
-		of a journal using the OJS XML generated
-		'''
-		try:
-			self.getVariables()
-			self.runImport()
-		except DataError as e:
-			return "Execution halted due to error {0}".format(e.strerror)
-		except subprocess.CalledProcessError as e:
-			return "System command failed {0}".format(e.cmd)
-		return None
+    def step(self):
+        '''
+        This script logs into the OJS server
+        and runs the PHP script for import 
+        of a journal using the OJS XML generated
+        '''
+        try:
+            self.getVariables()
+            self.runImport()
+        except DataError as e:
+            return "Execution halted due to error {0}".format(e.strerror)
+        except subprocess.CalledProcessError as e:
+            return "System command failed {0}".format(e.cmd)
+        return None
 
-	def getVariables(self):
-		'''
-		This script pulls in all the variables
-		from the command line and the config file 
-		that are necessary for its running.
-		Errors in variables will lead to an 
-		Exception being thrown.
-		We need the ojs server, and user 
-		path to the correct dir for the import tool
-		path to the correct dir for the xml
-		name of the journal and an OJS admin user
-		'''
-		self.ojs_server = self.getConfigItem('ojs_server')
-		self.ojs_server_user = self.getConfigItem('ojs_server_user')
-		self.ojs_app_user = self.getConfigItem('ojs_app_user')
-		self.tool_path = self.getConfigItem('tool_path')
+    def getVariables(self):
+        '''
+        This script pulls in all the variables
+        from the command line and the config file 
+        that are necessary for its running.
+        Errors in variables will lead to an 
+        Exception being thrown.
+        We need the ojs server, and user 
+        path to the correct dir for the import tool
+        path to the correct dir for the xml
+        name of the journal and an OJS admin user
+        '''
+        self.ojs_server = self.getConfigItem('ojs_server')
+        self.ojs_server_user = self.getConfigItem('ojs_server_user')
+        self.ojs_app_user = self.getConfigItem('ojs_app_user')
+        self.tool_path = self.getConfigItem('tool_path')
 
-		anchor_name = self.getConfigItem('metadata_goobi_anchor_file', None, 'process_files')
-		anchor_data = goobi_tools.getAnchorFileData(\
-			os.path.join(self.command_line.process_path, anchor_name), ['TitleDocMainShort'])
-		self.volume_title = tools.parseTitle(anchor_data['TitleDocMainShort'])
+        process_path = self.command_line.process_path
+        mets_file_name = self.getConfigItem('metadata_goobi_file', None, 'process_files')
+        mets_file = os.path.join(process_path, mets_file_name)
+        issue_data = mets_tools.getIssueData(mets_file)
+        self.volume_title = tools.parseTitle(issue_data['TitleDocMainShort'])
 
-		# build the path to the ojs xml file based in the form 
-		# <upload_dir>/<journal_name>/<process_name>/<process_name>.xml
-		upload_dir = self.getConfigItem('upload_dir').\
-			format(self.volume_title, self.command_line.process_title)
+        # build the path to the ojs xml file based in the form 
+        # <upload_dir>/<journal_name>/<process_name>/<process_name>.xml
+        upload_dir = self.getConfigItem('upload_dir').\
+            format(self.volume_title, self.command_line.process_title)
 
-		xml_name = "{0}.xml".format(self.command_line.process_title)
-		self.xml_path = os.path.join(upload_dir,  xml_name)
+        xml_name = "{0}.xml".format(self.command_line.process_title)
+        self.xml_path = os.path.join(upload_dir,  xml_name)
 
-	def runImport(self):
-		'''
-		Using the supplied variables - call the script on the OJS server through ssh
-		throws a CalledProcessError if the script failed.
+    def runImport(self):
+        '''
+        Using the supplied variables - call the script on the OJS server through ssh
+        throws a CalledProcessError if the script failed.
 
-		NOTE - this script depends on the Goobi user (tomcat_user) being able to 
-		ssh into the OJS server without password authentication, i.e. through a 
-		public/private key setup. See the wiki for more details.
-		'''
-		login = "{0}@{1}".format(self.ojs_server_user, self.ojs_server)
-		subprocess.check_call(['ssh', login, 'sudo', 'php', self.tool_path, 
-			'NativeImportExportPlugin', 'import', self.xml_path, self.volume_title, self.ojs_app_user])
-		
+        NOTE - this script depends on the Goobi user (tomcat_user) being able to 
+        ssh into the OJS server without password authentication, i.e. through a 
+        public/private key setup. See the wiki for more details.
+        '''
+        login = "{0}@{1}".format(self.ojs_server_user, self.ojs_server)
+        subprocess.check_call(['ssh', login, 'sudo', 'php', self.tool_path, 
+            'NativeImportExportPlugin', 'import', self.xml_path, self.volume_title, self.ojs_app_user])
+        
 
 if __name__ == '__main__':
-	
-	RunOJSImport().begin()
+    
+    RunOJSImport().begin()
