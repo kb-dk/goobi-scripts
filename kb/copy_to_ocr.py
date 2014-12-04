54957b9d08b81c068924089e0c7eeeefb56283b0
lengro <lengroe@gmail.com>
2014-12-04 14:59:02 +0100

    Corrected path names

--- kb/copy_to_ocr.py
+++ kb/copy_to_ocr.py
@@ -15,7 +15,7 @@ class CopyToOcr( Step ):
         self.name = 'Kopiering af billeder til OCR'
         self.config_main_section = 'copy_to_ocr'
         self.ocr_output_section = 'copy_from_ocr'
-        self.valid_exts_section = 'move_invalid_files'
+        self.valid_exts_section = 'valid_file_exts'
         self.folder_structure_section = 'process_folder_structure'
         self.essential_config_sections.update([self.folder_structure_section, 
                                                self.folder_structure_section,
@@ -43,7 +43,7 @@ class CopyToOcr( Step ):
         # ======================================================================
         # legr: Get the correct OCR server for the issue - antikva or fraktur
         # ======================================================================
-        ocr_workflow_type = self.getSetting('ocr_workflow_type') # legr: how do I get this from Goobi?
+        ocr_workflow_type = self.getSetting('ocr_workflow_type')
         if ocr_workflow_type == 'antikva':
             # legr: currently antikva on ocr-01
             ocr_transitfolder = self.getSetting('ocr_antikva_transit')
@@ -52,27 +52,20 @@ class CopyToOcr( Step ):
             # legr: currently fraktur on ocr-02
             ocr_transitfolder = self.getSetting('ocr_fraktur_transit')
             ocr_hotfolder = self.getSetting('ocr_fraktur_hotfolder')
+
         self.transit_dir = os.path.join(ocr_transitfolder,process_title)
         self.hotfolder_dir = os.path.join(ocr_hotfolder,process_title)
 
         self.sleep_interval = int(self.getConfigItem('sleep_interval'))
         self.retries = int(self.getConfigItem('retries'))
         
+        # legr: 'doc_pdf_color_path' for now, we also have defined a path for bw
         self.goobi_pdf = os.path.join(process_path, 
-            self.getConfigItem('doc_limbpdf_path', None, 'process_folder_structure'))
+            self.getConfigItem('doc_pdf_color_path', None, 'process_folder_structure'))
         self.valid_exts = self.getConfigItem('valid_file_exts',None, self.valid_exts_section).split(';')
         input_files = self.getConfigItem('img_master_path',None, self.folder_structure_section) 
         self.input_files = os.path.join(process_path,input_files)
-        
-        #=======================================================================
-        # Get the correct LIMB workflow hotfolder for the issue - BW or Color
-        #=======================================================================
-        #limb_workflow_type = self.getSetting('limb_workflow_type')
-        #if limb_workflow_type == 'bw':
-        #    limb_hotfolder = self.getSetting('limb_bw_hotfolder')
-        #elif limb_workflow_type == 'color':
-        #    limb_hotfolder = self.getSetting('limb_color_hotfolder')
-        #self.hotfolder_dir = os.path.join(limb_hotfolder,process_title)
+
 
     def step(self):
         error = None
@@ -81,12 +74,6 @@ class CopyToOcr( Step ):
         msg = msg.format(self.source_folder, self.hotfolder_dir, self.transit_dir)
         self.debug_message(msg)
         try:
-            """
-            legr:
-            source = {processpath} + "images/master_orig/"
-            transit = "/tmp/ocr-02/InputFolder/dod-transit/" + {processtitle} # tmp to replaced with mnt when prod-ready
-            dest =
-            """
             tools.copy_files(source = self.source_folder,
                              dest = self.hotfolder_dir,
                              transit = self.transit_dir,
