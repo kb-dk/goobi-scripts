f6755fb7c6a867d1f88736cc01e3df524b7debb1
lengro <lengroe@gmail.com>
2015-03-11 12:25:14 +0100

    Removed unused method argument. Testing message system

--- kb/count_image_files.py
+++ kb/count_image_files.py
@@ -10,7 +10,7 @@ import os
 
 
 class CountImageFiles(Step):
-    def setup(self, _):
+    def setup(self):
         self.name = 'Find antal billeder og skriv resultat i Goobi'
         self.config_main_section = 'count_image_files'
         self.valid_exts_section = 'valid_file_exts'
@@ -27,7 +27,7 @@ class CountImageFiles(Step):
         # Get the property name we have defined in goobi_processProperties.xml (from config.ini)
         self.property_name = self.getConfigItem('property_name')
 
-    def step(self, _):
+    def step(self):
         error = None
         try:
             self.getVariables()
@@ -36,9 +36,12 @@ class CountImageFiles(Step):
             self.info_message("Der blev optalt {} billeder".format(image_count))
             # write the number of images to a Goobi property
             if self.goobi_com.addProperty(name=self.property_name, value=image_count, overwrite=True):
-                self.info_message("Succes: Billedantallet er nu gemt i Goobi")
+                self.info_message("Succes: Billedantallet er nu gemt i Goobi (i)")
+                self.warning_message("Succes: Billedantallet er nu gemt i Goobi (w)")
+                self.error_message("Succes: Billedantallet er nu gemt i Goobi (e)")
+                self.debug_message("Succes: Billedantallet er nu gemt i Goobi (d)")
             else:
-                self.info_message("Advarsel:  Billedantallet blev muligvis ikke gemt korrekt i Goobi")
+                self.error_message("Advarsel:  Billedantallet blev muligvis ikke gemt korrekt i Goobi")
         # not sure which exceptions to expect...
         except ValueError as e:
             error = str(e.with_traceback)
